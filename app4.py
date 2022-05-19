4f2bd089c9b39211d0fde1e9234b3fb5ba81c385
tamnalee <105832393+tamnalee@users.noreply.github.com>
2022-05-19 15:34:06 +0900

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,10 +1,41 @@
 import streamlit as st
 import pandas as pd
 
-
 def main():
-    pass
+    df= pd.read_csv('iris.csv')
+    # 버튼 만들기
+
+    #True가 됨
+    # if st.button('데이터 보기'): 
+    #     st.dataframe(df)
+    
+    # 대문자 버튼을 만들고 버튼을 누르면 species 컬럼의 값들을 대문자로 변경
+
+    st.dataframe(df)
+
+    if st.button('대문자'):
+        df['species'] = df['species'].str.upper()
+        st.dataframe(df)
+
+    # 라디오 버튼: 여러개중 한개를 선택할때
+    my_order = ['오름차순 정렬','내림차순 정렬']  # 리스트를 먼저 만듦
+
+    status =  st.radio('정렬방법 선택', my_order)
+
+    if status == my_order[0]:   # petal_length 를 오름차순으로
+        st.dataframe(df.sort_values('petal_length'))  # 정렬한것을 메모리에 저장해야 화면에 나오게 된다.
+    elif status == my_order[1]:
+        df.sort_values('petal_length', ascending=False, inplace= True)
+        st.dataframe(df)
 
 
-if __name__=='__main__':
+   
+
+
+   
+
+
+
+
+if __name__ == '__main__':
     main()
